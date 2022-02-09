b39f8ae1afc831a596c40774343711d18cd92457
Rasik <98209882+Wlad1slav@users.noreply.github.com>
2022-02-09 17:42:49 +0200

    реорганизация

---
+++ .vscode/avr.properties.mk
@@ -0,0 +1,4 @@
+AVR.source.compiler = d:\avr-gcc-11.1.0-x64-windows\bin\avr-gcc.exe
+AVR.source.libraries = d:\avr-gcc-11.1.0-x64-windows\lib d:\avr-gcc-11.1.0-x64-windows\bin d:\avr-gcc-11.1.0-x64-windows\libexec D:\avr-gcc-11.1.0-x64-windows D:\avr-gcc-11.1.0-x64-windows\avr D:\avr-gcc-11.1.0-x64-windows\include D:\avr-gcc-11.1.0-x64-windows\share D:\avr-gcc-11.1.0-x64-windows\avr\include\avr
+AVR.device.type = atmega8515
+AVR.device.frequency = 100000UL
