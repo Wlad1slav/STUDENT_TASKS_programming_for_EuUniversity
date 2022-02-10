2a48c5fb0eeed2e6ff613fa2a2993d3cc883dc78
Rasik <98209882+Wlad1slav@users.noreply.github.com>
2022-02-10 15:48:41 +0200

    5-8 (while)

--- ООП/Завдання на цикл while/main.py
+++ ООП/Завдання на цикл while/main.py
@@ -1,6 +1,10 @@
 import WhileModule as MMM
 
-MMM.squares(10) #1
+# MMM.squares(10) #1
 # MMM.minimum_divide(81) #2
 # MMM.list_squares(100) #3
 # MMM.exact_pow_two(4) #4
+# MMM.logarithm(7)
+# MMM.morning_running(10, 20)
+# MMM.bank(100, 10, 200)
+MMM.Fibonachi(6)
