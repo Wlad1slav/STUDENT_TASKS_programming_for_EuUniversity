9f6680394b0d454c126ed746f9fd332be919ac6f
Rasik <98209882+Wlad1slav@users.noreply.github.com>
2022-02-16 10:23:10 +0200

    5-9 (for)

--- ООП/Оператор for/forLib.py
+++ ООП/Оператор for/forLib.py
@@ -1,3 +1,5 @@
+import math
+
 ################
 # Оператор for #
 ################
@@ -32,6 +34,67 @@ def power(N):
             pow *= 2
         print(pow)
             
+##################################
+# Завдання 4. 1/0!+1/1!+1/2!+... #
+##################################
+# def beliberda(N):
+#     # sum = 1; factorial = 1; float(factorial); float(sum)
+#     for i in range(N):
+#         I = i + 2
+#         # for fact in range(I):
+#             # if fact > 1: factorial *= (fact - 1)
+#             # print(factorial)
+#         # sum += (1 / factorial)
+#     print(sum)
+
+#######################
+# Завдання 5. Ступінь #
+#######################
+def power_again(a, n): print(a**n)
+
+#########################
+# Завдання 6. Факторіал #
+#########################
+def factorial_again(N):
+    factor = 1; N += 2 #кастыль
+    if N - 2 <= 12:
+        for i in range(N):
+            if i > 1: factor *= (i - 1)
+            
+    print(f"{N-2}! = {factor}")
+    
+###########################
+# Завдання 7. Парні числа #
+###########################
+def even_numbers(a,b):
+    if a < b:
+        for a in range(b+1):
+            if a % 2 == 0 and a != 0: print(a, end=' ')
+        print() #чтобы путь к файлу выводился на следующей точке
+    
+#######################
+# Завдання 8. Залишок #
+#######################
+def remainder(a, b, c, d):
+    if a < b:
+        for a in range(b+1):
+            if a % d == c and a != 0: print(a, end=' ')
+        print()
+
+########################
+# Завдання 9. Квадрати #
+########################
+def squares(a, b):
+    if a < b:
+        for a in range(b+1):
+            if a != 0 and a != 1 and a % (a ** 0.5) == 0: print(a) #a ** 0.5 - корень числа
+
+##############################
+# Завдання 10. Цифра в числі #
+##############################
+#-
+
+
 
 
 if __name__ == "__main__": 
--- ООП/Оператор for/main.py
+++ ООП/Оператор for/main.py
@@ -2,4 +2,10 @@ import forLib as MMM
 
 # MMM.sumSq(99)
 # MMM.factorial(12)
-MMM.power(4)
+# MMM.power(4)
+# MMM.beliberda(2)
+# MMM.power_again(3, 3)
+# MMM.factorial_again(2)
+# MMM.even_numbers(2, 56)
+# MMM.remainder(2, 12, 0, 2)
+MMM.squares(2, 9)
